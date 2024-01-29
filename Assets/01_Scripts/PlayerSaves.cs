444df3bd6d0ae9b432f53a929fa47518c3270d87
Raúl Oyarzún Becerra <r.oyarzun83@gmail.com>
2024-01-29 19:46:53 -0300

    Ventana Item

--- Assets/01_Scripts/PlayerSaves.cs
+++ Assets/01_Scripts/PlayerSaves.cs
@@ -18,4 +18,30 @@ public class PlayerSaves : MonoBehaviour
     {
         
     }
+
+    public void SaveData()
+    {
+        float health = GameManager.Instance.health;
+        float maxHealth = GameManager.Instance.maxHealth;
+
+        PlayerPrefs.SetFloat("Health", health);
+        PlayerPrefs.SetFloat("MaxHealth", maxHealth);
+
+        
+    }
+
+    public void LoadData()
+    {
+        PlayerPrefs.GetFloat("Health");
+        PlayerPrefs.GetFloat("MaxHealth");
+    }
+
+    public void ResetData()
+    {
+
+        PlayerPrefs.SetFloat("Health", 30);
+        PlayerPrefs.SetFloat("MaxHealth", 40);
+
+
+    }
 }
